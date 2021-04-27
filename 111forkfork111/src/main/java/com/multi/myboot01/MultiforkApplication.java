906d5f1d0b691a4cc88f82312b0a684d5ad6b6ee
da hhwon <da-hhwon@gmail.com>
2021-04-27 15:05:12 +0900

    first commit

---
+++ 111forkfork111/src/main/java/com/multi/myboot01/MultiforkApplication.java
@@ -0,0 +1,13 @@
+package com.multi.myboot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiforkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiforkApplication.class, args);
+	}
+
+}
